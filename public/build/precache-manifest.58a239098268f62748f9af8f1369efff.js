40e5ac85121e5a8da28ca8dab6a2076b97866ead
Marc Dwyer <envdia@gmail.com>
2018-12-21 16:25:52 -0600

    added files

---
+++ public/build/precache-manifest.58a239098268f62748f9af8f1369efff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9bcb671d5328129ee68f",
+    "url": "/static/js/main.9bcb671d.chunk.js"
+  },
+  {
+    "revision": "a06103daa5160c337494",
+    "url": "/static/js/1.a06103da.chunk.js"
+  },
+  {
+    "revision": "9bcb671d5328129ee68f",
+    "url": "/static/css/main.ded7ef21.chunk.css"
+  },
+  {
+    "revision": "4821446d5a2fea68800233eeca2962da",
+    "url": "/index.html"
+  }
+];
