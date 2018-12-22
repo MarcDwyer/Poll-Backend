8d05eb55a61708a280c6165235736cf438a18e4e
Marc Dwyer <envdia@gmail.com>
2018-12-22 08:51:03 -0600

    wss

---
+++ public/build/precache-manifest.b9742c8d27e34d0501e7235727589f97.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "571f43f6c1b143a6027e",
+    "url": "/static/js/main.571f43f6.chunk.js"
+  },
+  {
+    "revision": "a06103daa5160c337494",
+    "url": "/static/js/1.a06103da.chunk.js"
+  },
+  {
+    "revision": "571f43f6c1b143a6027e",
+    "url": "/static/css/main.ded7ef21.chunk.css"
+  },
+  {
+    "revision": "733aab2145b465e0fab4ee0c56f8d87a",
+    "url": "/index.html"
+  }
+];
