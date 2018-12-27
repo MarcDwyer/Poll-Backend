724a6c36a4d6f8be281602a500e1bab15ec0ede8
Marc Dwyer <envdia@gmail.com>
2018-12-26 20:31:23 -0600

    added fa fa

---
+++ public/build/precache-manifest.3f43e21ad83a0a81f27daee1f6ac6571.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "573e185dcf556db6fda7",
+    "url": "/static/js/main.573e185d.chunk.js"
+  },
+  {
+    "revision": "11159d1c3437cad2923e",
+    "url": "/static/js/1.11159d1c.chunk.js"
+  },
+  {
+    "revision": "573e185dcf556db6fda7",
+    "url": "/static/css/main.513458c8.chunk.css"
+  },
+  {
+    "revision": "d13cb68ca18d346138ad7c34e1504132",
+    "url": "/index.html"
+  }
+];
